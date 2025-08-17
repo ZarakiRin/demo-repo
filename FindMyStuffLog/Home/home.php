03b4caa0f7e06f4035ff1f70d8f9c4f0d4b6c5ff
Jeral <rheynanbelloco23@gmail.com>
2025-08-18 00:16:08 +0800

    update the title

--- FindMyStuffLog/Home/home.php
+++ FindMyStuffLog/Home/home.php
@@ -28,7 +28,7 @@ while ($row = $result->fetch_assoc()) {
     <link rel="stylesheet" href="../Home/Home.css">
     <link rel="stylesheet" href="../Dashboard/dashboard.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
-    <title>Users ni je</title>
+    <title>Users</title>
 </head>
 <body>
     <?php include '../Dashboard/dashboard.php'; ?>
